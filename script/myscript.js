d7e1c21f25bd854b141bfe9fe0af548d73befb76
Marco-Vigilio <tonyvigilio@gmail.com>
2023-06-03 12:44:18 +0200

    stampo a schermo scritta h1 + imagine con Vue

--- script/myscript.js
+++ script/myscript.js
@@ -8,3 +8,15 @@
  * 
  */
 
+let createApp = Vue.createApp; //STESSA IDENTICA COSA MA SCRITTA IN MODO DIFFERENTE DI SOTTO
+//let { createApp } = Vue;  //DESTRUTTURAZIONE DELLA VARIABILE SOPRA
+
+createApp({
+    data(){
+        return {
+            message: "Your Welcome in Vue",
+            image: "https://unsplash.it/300/300?image",
+        };
+        
+    }
+}).mount("#app");
